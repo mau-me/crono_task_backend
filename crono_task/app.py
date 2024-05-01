b37c1c5ded0360077b9c89f58af90ceccccbe288
mau-me <mauriciosm95@gmail.com>
2024-05-01 20:39:45 -0300

    feat: add schema base com pydantic

--- crono_task/app.py
+++ crono_task/app.py
@@ -1,15 +1,19 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from crono_task.schemas import Message
 
 app = FastAPI()
 
 # AAA - Arrange, Act, Assert
 
 
-@app.get('/')
+@app.get('/', response_model=Message, status_code=HTTPStatus.OK.value)
 def read_root():
     return {'message': 'Olá Mundo!'}
 
 
-@app.get('/{name}')
+@app.get('/{name}', response_model=Message, status_code=HTTPStatus.OK.value)
 def read_name(name: str):
     return {'message': f'Olá {name}!'}
