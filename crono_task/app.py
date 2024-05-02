a6ed31fa24505e9146c034345de590a7c5f2b014
mau-me <mauriciosm95@gmail.com>
2024-05-02 14:50:21 -0300

    feat: cria o crud base da aplicação

--- crono_task/app.py
+++ crono_task/app.py
@@ -1,19 +1,84 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from crono_task.schemas import Message
+from crono_task.schemas import (
+    Message,
+    UserDB,
+    UserList,
+    UserResponseSchema,
+    UserSchema,
+)
 
 app = FastAPI()
+
+database = []
 
 # AAA - Arrange, Act, Assert
 
 
-@app.get('/', response_model=Message, status_code=HTTPStatus.OK.value)
+@app.get('/', response_model=Message, status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Olá Mundo!'}
 
 
-@app.get('/{name}', response_model=Message, status_code=HTTPStatus.OK.value)
-def read_name(name: str):
-    return {'message': f'Olá {name}!'}
+@app.post(
+    '/users', response_model=UserResponseSchema, status_code=HTTPStatus.CREATED
+)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users', response_model=UserList, status_code=HTTPStatus.OK)
+def read_users():
+    return {'users': database}
+
+
+@app.get('/users/{user_id}', response_model=UserResponseSchema)
+def read_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_index = user_id - 1
+
+    user = database[user_index]
+
+    return user
+
+
+@app.put(
+    '/users/{user_id}',
+    response_model=UserResponseSchema,
+    status_code=HTTPStatus.OK,
+)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_index = user_id - 1
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_index] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_index = user_id - 1
+    del database[user_index]
+
+    return {'message': 'User deleted'}
--- crono_task/schemas.py
+++ crono_task/schemas.py
@@ -1,5 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Message(BaseModel):
     message: str
+
+
+class UserSchema(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+
+class UserResponseSchema(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+
+
+class UserDB(UserSchema):
+    id: int
+
+
+class UserList(BaseModel):
+    users: list[UserResponseSchema]
