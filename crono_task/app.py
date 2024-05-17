88dd7437ce426db687d13a7776846c5d0c45e350
mau-me <mauriciosm95@gmail.com>
2024-05-17 12:42:16 -0300

    feat: Add CORS middleware to allow cross-origin requests
    This commit adds the CORS middleware to the FastAPI application in order to allow cross-origin requests. The middleware is configured to allow all origins, credentials, methods, and headers.

--- crono_task/app.py
+++ crono_task/app.py
@@ -2,11 +2,21 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from crono_task.routers import auth, todos, users
-from crono_task.schemas import Message
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+
+)
+
 
 app.include_router(auth.router)
 app.include_router(users.router)
